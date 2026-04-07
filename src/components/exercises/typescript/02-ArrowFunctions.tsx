082a6f5c9f635184b1807a56fa8e3867d77f729a
Daniea1 <112690693+Daniea1@users.noreply.github.com>
2026-04-07 17:51:34 +0200

    Solved exercise 02

--- src/components/exercises/typescript/02-ArrowFunctions.tsx
+++ src/components/exercises/typescript/02-ArrowFunctions.tsx
@@ -2,16 +2,37 @@ import { ExerciseCard, OutputBox } from "@/components/exercises/ExerciseCard";
 
 export default function ArrowFunctions() {
   // TODO: Write a regular function `add(a, b)` that returns a + b.
+  function add(a: number, b: number): number {
+    return a + b;
+  }
 
   // TODO: Rewrite it as an arrow function with an explicit body { return ... }.
+  const add_v2 = (a: number, b: number): number => {
+    return a + b;
+  }
 
   // TODO: Rewrite it again as an arrow function with an implicit return (no braces, no return keyword).
+  const add_v3 = (a: number, b: number): number => a + b;
 
   // TODO: Write an arrow function `double` that takes one number and doubles it.
+
+  const double = (a: number): number => {
+    return a * 2;
+  }
 
   // TODO: Create an object `timer` with a `label` property and a `getLabel()` method.
   //       Inside getLabel, use an arrow function that returns `this.label`.
   //       Call getLabel() and store the result.
+
+  const timer = {
+    label: "hey",
+    getLabel() {
+      const arrow = () => this.label;
+      return arrow();
+    }
+  }
+
+  const timerResult = timer.getLabel();
 
   return (
     <ExerciseCard
@@ -24,11 +45,11 @@ export default function ArrowFunctions() {
     >
       <OutputBox
         entries={[
-          // TODO: { label: "regular add(3, 4)", value: ??? },
-          // TODO: { label: "arrow add(3, 4)", value: ??? },
-          // TODO: { label: "implicit add(3, 4)", value: ??? },
-          // TODO: { label: "double(5)", value: ??? },
-          // TODO: { label: "timer.getLabel() [lexical this]", value: ??? },
+          { label: "regular add(3, 4)", value: add(3, 4) },
+          { label: "arrow add(3, 4)", value: add_v2(3, 4) },
+          { label: "implicit add(3, 4)", value: add_v3(3, 4) },
+          { label: "double(5)", value: double(5) },
+          { label: "timer.getLabel() [lexical this]", value: timerResult },
         ]}
       />
     </ExerciseCard>
